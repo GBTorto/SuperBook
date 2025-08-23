839bd65cf49fe293100afc4a214c3a65a81cf927
unknown <moraisgabriel867@gmail.com>
2025-08-23 14:33:07 -0300

    Criação do banco de dados.

--- heroes/models.py
+++ heroes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Hero():
+class Hero(models.Model):
     codinome = models.CharField(max_length=50, unique=True)
     nome_real = models.CharField(max_length=100, blank=True, null=True)
     poder_principal = models.CharField(max_length=100)
