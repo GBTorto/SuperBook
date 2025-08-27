39cc6f7eaaa2192cfa8adbdc8de507ad51c4a338
GBTorto <moraisgabriel867@gmail.com>
2025-08-27 14:47:46 -0300

    criados FBV e CBV para o app Heroes.

--- heroes/urls.py
+++ heroes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import HeroListView
 
 urlpatterns = [
-    path('lista/', views.lista_herois, name='lista_herois'),
+    # path('lista/', views.lista_herois, name='lista_herois'),
+    path('cbv-lista/', HeroListView.as_view(), name='cbv_lista_herois'),
 ]
 
--- heroes/views.py
+++ heroes/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from .models import Hero
+from django.views.generic import ListView
 
-def lista_herois(request):
-    herois = Hero.objects.all()  # busca todos os heróis do banco
-    return render(request, "heroes/lista_herois.html", {"herois": herois})
+
+class HeroListView(ListView):
+    model = Hero
+    template_name = "heroes/lista_herois.html"
+    context_object_name = "herois"
+
+
+
+# def lista_herois(request):
+#     herois = Hero.objects.all()  # busca todos os heróis do banco
+#     return render(request, "heroes/lista_herois.html", {"herois": herois})
 
